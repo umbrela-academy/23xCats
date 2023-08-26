0d37b294472f5b17925f2c51ae5eae57db89a1ef
Risav <git@risav.dev>
2023-08-26 12:51:51 +0545

    feat(best): add test proj for devicescript

--- packages/mt/src/app/core/algebra/function/function.defs.ts
+++ packages/mt/src/app/core/algebra/function/function.defs.ts
@@ -1,10 +1,15 @@
 export type UnaryFunc<T, S> = (t: T) => S;
-export type BinaryFunc<T, S> = UnaryFunc<T, UnaryFunc<T, S>>; //(t1: T) => UnaryFunc<T, S>; //UnaryFunc<T, UnaryFunc<T, S>
+export type BinaryFunc<T, S> = UnaryFunc<T, UnaryFunc<T, S>>; 
 export type TernaryFunc<T, S> = UnaryFunc<T, BinaryFunc<T,  S>>;
 export type QuarternaryFunc<T, S> = UnaryFunc<T, TernaryFunc<T,  S>>;
-export type MAryFunc<T, S> = UnaryFunc<T, NAryFunc<T,  S>>;
+export type MAryFunc<T, S> = UnaryFunc<T, NAryFunc<S>>;
 
 export type OneAryFunc<T, S> = UnaryFunc<T , S>;
 export type TwoAryFunc<T, S, U> = (t: T, s: S) => U;
 export type ThreeAryFunc<T, S, U, V> = (t1: T, t2: S, t3: U) => V;
-export type NAryFunc<T, S> = (...t: T[]) => S;  
+export type NAryFunc<S> = (...t: any[]) => S;  
+
+export type Church1<T> = (u: T) => T
+export type Church2<T> = Church1<Church1<T>>;
+export type Church3<T> = Church1<Church2<T>>;
+export type ChurchN<T> = NAryFunc<T>;
--- packages/mt/src/app/core/domain/model/tuple/index.ts
+++ packages/mt/src/app/core/domain/model/tuple/index.ts
@@ -1,12 +1,16 @@
+import { UnaryFunc } from '../../../algebra/function/function.defs';
 import { Qnion, Quarternion } from '../../../algebra/qnion/qnion.defs';
 
 export type None = null | undefined | void | never; //0
 
 export type Sngl<T> = T; //1
 export type Pair<T, U> = [Sngl<T>, Sngl<U>];
-export type Trpl<T, U, V> = [Sngl<T>, Pair<U, V>];
-export type Quad<T, U, V, W> = [Sngl<T>,Trpl< U, V, W>];
-export type PenT<T, U, V, W, X> = [Sngl<T>, Quad<U, V, W, X>]; //5
+export type TwoNaryFunc<T, U> = UnaryFunc<Pair<T, T>, U>;
+export type Trpl<T, U, V> = Pair<Sngl<T>, Pair<U, V>>;
+export type ThreeNaryFunc<T,U> = UnaryFunc<Trpl<T,T,T>, U>;
+export type Quad<T, U, V, W> = Pair<Sngl<T>,Trpl< U, V, W>>;
+export type FourNaryFunc<T,U> = UnaryFunc<Quad<T,T,T,T>, U>;
+export type PenT<T, U, V, W, X> = Pair<Sngl<X>, Quad<T,U, V, W>>; //5
 export type SexT<T, U, V, W, X, Y> = [Sngl<T>, PenT<U, V, W, X, Y>]; //6
 export type SepT<T, U, V, W, X, Y, Z> = [Sngl<T>, SexT<U, V, W, X, Y, Z>]; //7s
 export type OctT<T, U, V, W, X, Y, Z, A> = [Sngl<T>, SepT<U, V, W, X, Y, Z, A>]; //7s
@@ -46,10 +50,15 @@ export type Tuple<T, U, V, W, X, Y, Z, A> =
   | Quad<T, U, V, W> //4
   | OctT<T, U, V, W, X, Y, Z, A>; //8
 
-export type QnionTUTime<T, U, Time> = Qnion<T, U, Time>;
-export type QnionTUVTime<T, U, V, Time> = Qnion<T, [U, V], Time>;
-export type QnionTUVWTime<T, U, V, W, Time> = Qnion<T, [U, V, W], Time>;
+// Onions of time. How much imagination do you have?
+export type QnionTUTime<T, U, Time> = Qnion<T, U, Time>; // time with 1 U imaginary D
+export type QnionTUVTime<T, U, V, Time> = Qnion<T, [U, V], Time>; // time with 2 U, V imaginary D
+export type QnionTUVWTime<T, U, V, W, Time> = Qnion<T, [U, V, W], Time>; // time with 3 U, V, W imaginary D
 export type QnionTUVWXYZATime<T, U, V, W, X, Y, Z, A, Time> = Qnion<T, [U, V, W, X, Y, Z, A], Time>;
+
+
+
+
 
 /**
  * There is no notion of time alone without space in this system.
@@ -60,16 +69,28 @@ export type QnionTUVWXYZATime<T, U, V, W, X, Y, Z, A, Time> = Qnion<T, [U, V, W,
  * where TimeD = Quaternion
 */
 export type Time<T, U, V, W, X, Y, Z, A> =
-  | QnionTUTime<T, U, Quarternion>
-  | QnionTUVTime<T, U, V, Quarternion>
-  | QnionTUVWTime<T, U, V, W, Quarternion>
-  | QnionTUVWXYZATime<T, U, V, W, X, Y, Z, A, Quarternion>;
-export type Time2S = QnionTUTime<number, number, Quarternion>;
-export type Time3S = QnionTUVTime<number, number, number, Quarternion>;
-export type Time4S = QnionTUVWTime<number, number, number, number, Quarternion>;
+  | QnionTUTime<T, U, Quarternion> // Pair T U
+  | QnionTUVTime<T, U, V, Quarternion> // Trpl T U V
+  | QnionTUVWTime<T, U, V, W, Quarternion> // Quad T U V W
+  | QnionTUVWXYZATime<T, U, V, W, X, Y, Z, A, Quarternion>; // OctT T, U, V, W, X, Y, Z, A
+
+
+  
+
+
+export type Time2S = QnionTUTime<
+    number, // real r
+    number, // imaginary i
+    Quarternion
+>;
+
+export type Time3S = QnionTUVTime<number, number, number, Quarternion>; // real r, imaginary i j 
+export type Time4S = QnionTUVWTime<number, number, number, number, Quarternion>; // real r, imaginary i j k
 export type Time8S = QnionTUVWXYZATime<
   number, number, number, number, number, number, number, number, Quarternion
->;
+>; // real r, imaginary i j k
+
+export type QMTime = Time4S;
 export type QTime = Time3S;
 export type NaiveQTime = Time2S;
 
@@ -106,6 +127,8 @@ export type Space3D = TUVSpace<number, number, number, Quarternion>;
 export type Space4D = TUVWSpace<number, number, number, number, Quarternion>;
 export type Space8D = TUVWASpace<number, number, number, number, number, number, number, number, Quarternion>;
 
+
+
 export type QPair<T, U, Time> =
   { value: Pair<T, U> } & TUSpace<T, U, Time>;
 export type QTrpl<T, U, V, Time> =
@@ -115,6 +138,11 @@ export type QQuad<T, U, V, W, Time> =
 export type QOctT<T, U, V, W, X, Y, Z, A, Time> =
   { value: OctT<T, U, V, W, X, Y, Z, A> } & TUVWASpace<T, U, V, W, X, Y, Z, A, Time>
 
+
+
+
+
+  
 /**
  * A QTuple may be one slice of a matrix representing either
  * 2, 3, 4 or 8 dimensional space with a Q
